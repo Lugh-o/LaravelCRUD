a622bfe1efd35ec81cde8b7033e39bb4b200383f
Lugh-o <lughfalcao@gmail.com>
2024-06-14 15:54:15 -0300

    Projeto finalizado

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Customer;
-use App\Http\Requests\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
 use Illuminate\Http\Request;
 use App\Filters\V1\CustomersFilter;
-
+use App\Http\Requests\V1\StoreCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 
 class CustomerController extends Controller
 {
@@ -20,21 +19,17 @@ class CustomerController extends Controller
     public function index(Request $request)
     {
         $filter = new CustomersFilter();
-        $queryItems = $filter->transform($request); //[['column', 'operator', 'value']]
+        $filterItems = $filter->transform($request); //[['column', 'operator', 'value']]
 
-        if(count($queryItems) == 0) {
-            return new CustomerCollection(Customer::paginate());
+        $includeInvoices = $request->query('includeInvoices');
+
+        $customers = Customer::where($filterItems);
+
+        if($includeInvoices) {
+            $customers = $customers->with('invoices');
         }
-        $customer = Customer::where($queryItems)->paginate();
-        return new CustomerCollection($customer->appends($request->query()));        
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return new CustomerCollection($customers->paginate()->appends($request->query()));        
     }
 
     /**
@@ -42,23 +37,20 @@ class CustomerController extends Controller
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        return new CustomerResource(Customer::create($request->all()));
     }
 
     /**
      * Display the specified resource.
      */
     public function show(Customer $customer)
-    {
-        return new CustomerResource($customer);
-    }
+    {   
+        $includeInvoices = request()->query('includeInvoices');
+        if($includeInvoices) {
+            return new CustomerResource($customer->loadMissing('invoices'));
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Customer $customer)
-    {
-        //
+        return new CustomerResource($customer);
     }
 
     /**
@@ -66,7 +58,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -10,8 +10,9 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 //api/v1
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function() {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'middleware' => 'auth:sanctum'], function() {
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('invoices', InvoiceController::class);
 
+    Route::post('invoices/bulk', ['uses' => 'InvoiceController@bulkStore']);
 });
